34bc728f7ed724517269dc59fc93e5f3a7c47d4e
afit08 <afitrabbani@gmail.com>
2022-04-22 13:40:07 +0700

    first commit

---
+++ server/controller/IndexController.js
@@ -0,0 +1,11 @@
+import UserController from "./UserController";
+import BootcampController from "./BootcampController";
+import Curriculum_reviewController from "./Curriculum_reviewController";
+import EditBatchController from "./EditBatchController";
+
+export default {
+  UserController,
+  BootcampController,
+  Curriculum_reviewController,
+  EditBatchController
+};
---
+++ server/routes/IndexRoute.js
@@ -0,0 +1,11 @@
+import UserRoute from "./UserRoute";
+import BootcampRoute from "./BootcampRoute";
+import Curriculum_reviewRoute from "./Curriculum_reviewRoute";
+import EditBatchRoute from "./EditBatchRoute"
+
+export default {
+  UserRoute,
+  BootcampRoute,
+  Curriculum_reviewRoute,
+  EditBatchRoute
+};
